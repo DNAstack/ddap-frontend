054ef18676919608418ddd4b11eb06e57af861b9
Max Barkley <max@dnastack.com>
2018-12-19 16:20:42 -0500

    Configure CORS allowed origins with defaults for angular dev setup.

--- src/main/java/com/dnastack/ddapfrontend/security/SecurityConfiguration.java
+++ src/main/java/com/dnastack/ddapfrontend/security/SecurityConfiguration.java
@@ -1,12 +1,30 @@
 package com.dnastack.ddapfrontend.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    @ConfigurationProperties("ddap.cors.origins")
+    public List<String> allowedOrigins() {
+        return new ArrayList<>();
+    };
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(
@@ -16,6 +34,18 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeExchange().pathMatchers("/**").permitAll()
                 .and()
+                .cors()
+                .and()
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins());
+        configuration.setAllowedMethods(singletonList("*"));
+        final UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", configuration);
+        return configurationSource;
     }
 }
