4a0cec21ff5211390898adc2211e92219227ddea
Harsh Dwivedi <hdwiv1@gmail.com>
2019-01-03 15:37:21 -0500

    Temp commit, work so far.

--- src/main/java/com/dnastack/ddapfrontend/DdapFrontendApplication.java
+++ src/main/java/com/dnastack/ddapfrontend/DdapFrontendApplication.java
@@ -2,13 +2,32 @@ package com.dnastack.ddapfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DdapFrontendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DdapFrontendApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
 
+        Process started = null;
+        try {
+            /* Check if this is a dev environment, if so, enable hot reloading build for frontend */
+            String frontEndBuildExecutablePath = System.getenv("FRONTEND_BUILD_EXECUTABLE_PATH");
+
+            if (!StringUtils.isEmpty(frontEndBuildExecutablePath)) {
+                ProcessBuilder processBuilder = new ProcessBuilder(frontEndBuildExecutablePath, "run build:dev");
+                started = processBuilder.start();
+            }
+            SpringApplication.run(DdapFrontendApplication.class, args);
+        } finally {
+            if (started != null) {
+                started.destroy();
+            }
+
+        }
+    }
 }
+
 
