199265451055c5d6b1eb42399d3fb79e48abaaf3
Harsh Dwivedi <hdwiv1@gmail.com>
2019-01-03 17:03:01 -0500

    Work so far...2

--- src/main/java/com/dnastack/ddapfrontend/DdapFrontendApplication.java
+++ src/main/java/com/dnastack/ddapfrontend/DdapFrontendApplication.java
@@ -4,27 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 @SpringBootApplication
 public class DdapFrontendApplication {
 
-    public static void main(String[] args) throws IOException {
+    static Process started = null;
 
-        Process started = null;
+    public static void main(String[] args) throws IOException {
+        File lockFile = new File("target/frontend.lock");
         try {
             /* Check if this is a dev environment, if so, enable hot reloading build for frontend */
             String frontEndBuildExecutablePath = System.getenv("FRONTEND_BUILD_EXECUTABLE_PATH");
 
-            if (!StringUtils.isEmpty(frontEndBuildExecutablePath)) {
-                ProcessBuilder processBuilder = new ProcessBuilder(frontEndBuildExecutablePath, "run build:dev");
+
+
+            if (!StringUtils.isEmpty(frontEndBuildExecutablePath) && (started == null) && (!lockFile.exists())) {
+                ProcessBuilder processBuilder = new ProcessBuilder(frontEndBuildExecutablePath, "run", "build:dev").inheritIO();
+                processBuilder.directory(new File("./angular"));
                 started = processBuilder.start();
+                lockFile.createNewFile();
             }
             SpringApplication.run(DdapFrontendApplication.class, args);
         } finally {
-            if (started != null) {
-                started.destroy();
-            }
+            Files.deleteIfExists(lockFile.toPath());
+
+//            if (started != null && started.isAlive()) {
+//                started.destroy();
+//            }
 
         }
     }
