0d9a4dd0dd7771f0af46ad5d22a44341927c36ab
Milan Panik <milan.panik1@gmail.com>
2019-01-22 17:04:44 +0100

    Use common entity service for config entities

--- angular/src/app/rules/rule.service.ts
+++ angular/src/app/rules/rule.service.ts
@@ -1,55 +1,14 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { pluck } from 'rxjs/operators';
 
-import { environment } from '../../environments/environment';
-
-const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
+import {ConfigEntityService} from '../shared/ConfigEntityService';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RuleService {
+export class RuleService extends ConfigEntityService {
 
-  constructor(private http: HttpClient) { }
-
-  get(params?): Observable<any[]> {
-    params = params || {};
-    params.persona = 'nci_researcher';
-
-    return this.http.get<any[]>(environment.ddapApiUrl + '/config', { params })
-      .pipe(
-        pluck('rules')
-      );
-  }
-
-  save(ruleChange: any): Observable<any> {
-    const params = {
-      persona: 'nci_researcher',
-    };
-    const ruleName = ruleChange.item.name;
-
-    return this.http.post(
-      environment.ddapApiUrl + '/config/' + ruleName,
-      ruleChange,
-      { params, headers }
-    );
-  }
-
-  update(rule: any): Observable<any> {
-    const params = {
-      persona: 'nci_researcher',
-    };
-    const ruleName = rule.name;
-    const ruleChange = {
-      item: rule,
-    };
-
-    return this.http.patch(
-      environment.ddapApiUrl + '/config/' + ruleName,
-      ruleChange,
-      { params, headers }
-    );
+  constructor(http: HttpClient) {
+    super(http, 'rules');
   }
 }
