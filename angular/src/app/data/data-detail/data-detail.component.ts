7424a835ec91189ffbaa9ce6e0537fc65b29686a
Milan Panik <milan.panik1@gmail.com>
2019-02-08 15:17:59 +0100

    Use single search endpoint for beacons

--- angular/src/app/data/data-detail/data-detail.component.ts
+++ angular/src/app/data/data-detail/data-detail.component.ts
@@ -4,18 +4,19 @@ import { flatMap } from 'rxjs/operators';
 
 import { ResourceService } from '../../resources/resource.service';
 import { ImagePlaceholderRetriever } from '../../shared/RandomImageRetriever';
-import { ResourceViewBeaconService } from '../../shared/ResourceViewBeaconService';
+import { ResourceBeaconService } from '../../shared/ResourceViewBeaconService';
 
 @Component({
   selector: 'ddap-resource-detail',
   templateUrl: './data-detail.component.html',
   styleUrls: ['./data-detail.component.scss'],
-  providers: [ImagePlaceholderRetriever, ResourceViewBeaconService],
+  providers: [ImagePlaceholderRetriever, ResourceBeaconService],
   // Easiest way to override inner component styles; https://stackoverflow.com/a/36225709
   encapsulation: ViewEncapsulation.None,
 })
 export class DataDetailComponent implements OnInit {
 
+  beaconResponse$: any = null;
   error: string = null;
   resource: any;
   views: any;
@@ -24,7 +25,7 @@ export class DataDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private resourceService: ResourceService,
     public randomImageRetriever: ImagePlaceholderRetriever,
-    public beaconService: ResourceViewBeaconService) {}
+    private beaconService: ResourceBeaconService) {}
 
   ngOnInit() {
     this.route.params.pipe(
@@ -33,16 +34,6 @@ export class DataDetailComponent implements OnInit {
       this.resource = resource;
       this.views = this.getViews(resource);
     });
-  }
-
-  private getViews(resource) {
-    return Object
-      .keys(resource.views)
-      .map((key) => {
-        return {
-          ...resource.views[key],
-        };
-      });
   }
 
   getAccess(viewName) {
@@ -56,6 +47,21 @@ export class DataDetailComponent implements OnInit {
         if (viewAccessUrl) {
           this.resource.views[viewName].url = `${viewAccessUrl}/o?access_token=${accessToken}`;
         }
+      });
+  }
+
+  queryBeacons(query, resource) {
+    this.beaconService.queryResourceBeacons(query, resource)
+      .subscribe(response => this.beaconResponse$ = response);
+  }
+
+  private getViews(resource) {
+    return Object
+      .keys(resource.views)
+      .map((key) => {
+        return {
+          ...resource.views[key],
+        };
       });
   }
 
