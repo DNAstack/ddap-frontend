35c30084a4f156283243b90ada50cfe142fcc636
Viliam Rockai <viliam.rockai@gmail.com>
2019-02-21 10:31:40 +0100

    DISCO-1953 cache resource labels

--- angular/src/app/data/data.service.ts
+++ angular/src/app/data/data.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { first, mergeMap, pluck } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+import { first, map, mergeMap, pluck, tap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -10,29 +11,50 @@ import { environment } from '../../environments/environment';
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  private cache: any = {};
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getName(resourceId: string): Observable<string> {
+    const resourceName = this.cache[resourceId];
+    if (!resourceName) {
+      return this.getResource(resourceId).pipe(
+        map((resourceDto: any) => resourceDto.ui.label)
+      );
+    }
+
+    return of(resourceName);
+  }
 
   get(params?): Observable<any[]> {
     params = params || {};
 
-    return this.http.get<any[]>(environment.damApiUrl + '/resources', { params })
+    const putIntoCache = (resourcesDto) => {
+      resourcesDto.forEach((resource) => {
+        this.cache[resource.name] = resource.ui.label;
+      });
+    };
+
+    return this.http.get<any[]>(environment.damApiUrl + '/resources', {params})
       .pipe(
-        pluck('resources')
+        pluck('resources'),
+        tap(putIntoCache)
       );
   }
 
-  getResource(resourceName: string): Observable<any> {
+  getResource(resourceId: string): Observable<any> {
     return this.get().pipe(
       mergeMap((dto: any) => dto),
-      first((resource: any) => resource.name === resourceName)
+      first((resource: any) => resource.name === resourceId)
     );
   }
 
   getAccessRequestToken(resource, view): Observable<any[]> {
-    const params = {
-    };
+    const params = {};
 
-    return this.http.get<any[]>(`${environment.damApiUrl}/resources/${resource}/views/${view}`, { params })
+    return this.http.get<any[]>(`${environment.damApiUrl}/resources/${resource}/views/${view}`, {params})
       .pipe(
         pluck('token')
       );
