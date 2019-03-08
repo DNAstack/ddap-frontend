111e599a66628d51e0332f35feb8c05e919b7f0d
Harsh Dwivedi <harsh@dnastack.com>
2019-03-08 13:57:07 -0500

    Add a fix.

--- e2e-tests/src/main/java/com/dnastack/ddap/AggregateResourceSearchApiE2eTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/AggregateResourceSearchApiE2eTest.java
@@ -12,35 +12,11 @@ import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 
-/**
- * 1. Read the realm json in basee2e class, call getRealmConfig in basee2e class, pass in parameter of name
- * of resource, list of view types and it will return to you the modified realm config.
- */
 public class AggregateResourceSearchApiE2eTest extends BaseE2eTest {
-
-
-    protected String modifyRealmTemplate(String realmConfig, String resourceName, String resourceFilePath) throws IOException {
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode realmConfigJsonRoot = mapper.readValue(realmConfig, JsonNode.class);
-
-        ObjectNode resources = (ObjectNode) realmConfigJsonRoot.get("resources");
-
-        String resourceString = loadTemplate(resourceFilePath);
-        JsonNode resourceJson = mapper.readValue(resourceString, JsonNode.class);
-
-        ((ObjectNode) resources).put(resourceName, resourceJson);
-        ((ObjectNode) realmConfigJsonRoot).put("resources", resources);
-
-        return realmConfigJsonRoot.toString();
-    }
-
 
     @Before
     public void setupRealm() throws IOException {
         String realmConfigString = loadTemplate("/com/dnastack/ddap/config.json");
-        realmConfigString = modifyRealmTemplate(realmConfigString, "testResource3", "/com/dnastack/ddap/resource.json");
-        realmConfigString = modifyRealmTemplate(realmConfigString, "testResource2", "/com/dnastack/ddap/resource2.json");
         setupRealmConfig("nci_researcher", realmConfigString);
     }
 
