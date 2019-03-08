587b8c49324a5aed2994b57d0e8c5a4ebac1b0fd
Viliam Rockai <viliam.rockai@gmail.com>
2019-03-08 12:04:47 +0100

    DISCO-2008 Separate editor for tests on resource create page

--- angular/src/app/admin/resources/resource-manage/resource-manage.component.ts
+++ angular/src/app/admin/resources/resource-manage/resource-manage.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
 
-import { ChangeDto, ItemDto } from '../../shared/change.dto';
+import { ChangeModel } from '../../shared/change.model';
+import { EntityModel } from '../../shared/entity.model';
 import { JsonEditorDefaults } from '../../shared/jsonEditorDefaults';
 import { ResourceService } from '../resources.service';
 
@@ -14,41 +15,58 @@ import { ResourceService } from '../resources.service';
 export class ResourceManageComponent implements OnInit {
 
   errorDto: any;
-  entity: ChangeDto;
+  item: any;
+  test: any;
 
   errorEditorOptions: JsonEditorOptions;
   itemEditorOptions: JsonEditorOptions;
-
-  @ViewChild('itemEditor')
-  entityEditor: JsonEditorComponent;
+  testEditorOptions: JsonEditorOptions;
 
   @ViewChild('errorEditor')
   errorEditor: JsonEditorComponent;
 
+  @ViewChild('itemEditor')
+  itemEditor: JsonEditorComponent;
+
+  @ViewChild('testEditor')
+  testEditor: JsonEditorComponent;
+
   constructor(public resourceService: ResourceService,
               private router: Router,
               private route: ActivatedRoute) {
-    this.itemEditorOptions = new JsonEditorDefaults();
     this.errorEditorOptions = new JsonEditorDefaults();
-    this.itemEditorOptions.mode = 'code';
+    this.itemEditorOptions = new JsonEditorDefaults();
+    this.testEditorOptions = new JsonEditorDefaults();
+
     this.errorEditorOptions.mode = 'code';
     this.errorEditorOptions.onEditable = () => false;
-    this.entity = new ChangeDto(new ItemDto(undefined), {});
+
+    this.itemEditorOptions.mode = 'code';
+    this.testEditorOptions.mode = 'code';
   }
 
   ngOnInit() {
   }
 
   save() {
-    this.resourceService.saveDto(this.entity).subscribe(path => {
-        this.router.navigate(['../..'], { relativeTo: this.route });
-      },
-      (errorDto) => {
-        this.errorDto = errorDto.error;
-      });
+    const entityModel = new EntityModel(this.item.name, this.item);
+    const change = new ChangeModel(entityModel, this.test);
+
+    this.resourceService.save(change).subscribe(
+      () => this.goToResourceList(),
+      ({error}) => this.errorDto = error
+    );
   }
 
   updateItemDto(event: any) {
-    this.entity = event;
+    this.item = event;
+  }
+
+  updateTestDto(event: any) {
+    this.test = event;
+  }
+
+  private goToResourceList() {
+    this.router.navigate(['../..'], {relativeTo: this.route});
   }
 }
