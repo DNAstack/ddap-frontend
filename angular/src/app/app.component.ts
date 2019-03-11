63cd367dfb6eb6f8819c44e7b6fb0d9033fbcffc
Viliam Rockai <viliam.rockai@gmail.com>
2019-03-11 17:50:36 +0100

    DISCO-2042 Introduce the realm selector/input

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 
 import { IdentityService } from './identity/identity.service';
@@ -17,6 +18,7 @@ export class AppComponent implements OnInit {
 
   constructor(public loader: LoadingBarService,
               private realmService: RealmService,
+              private router: Router,
               private identityService: IdentityService) {
 
   }
@@ -29,5 +31,9 @@ export class AppComponent implements OnInit {
     this.identityService.getProfile().subscribe(profile => {
       this.profile = profile;
     });
+  }
+
+  goToRealm(realm) {
+    this.router.navigate([realm]);
   }
 }
