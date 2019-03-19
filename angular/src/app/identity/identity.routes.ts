ce0d0c6a642cc521692bf05034185b6efa2d6e18
Milan Panik <milan.panik1@gmail.com>
2019-03-19 21:58:47 +0100

    DISCO-2026 move myidentity to identity

--- angular/src/app/identity/identity.routes.ts
+++ angular/src/app/identity/identity.routes.ts
@@ -3,6 +3,5 @@ import { Routes } from '@angular/router';
 import { IdentityComponent } from './identity.component';
 
 export const IDENTITY_ROUTES: Routes = [
-  // TODO https://dnastack.atlassian.net/browse/DISCO-2026
-  {path: 'myidentity', component: IdentityComponent},
+  { path: 'identity', component: IdentityComponent },
 ];
