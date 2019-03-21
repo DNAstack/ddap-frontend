23558e4ba08725b19f7639480ffd78c0ec23739a
Milan Panik <milan.panik1@gmail.com>
2019-03-21 10:53:01 +0100

    Update ic options forms

--- angular/src/app/admin/identity-concetrator/options/options.module.ts
+++ angular/src/app/admin/identity-concetrator/options/options.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { IdentityConcentratorSharedModule } from '../shared/shared.module';
 
-import { OptionDetailComponent } from './option-detail/option-detail.component';
 import { OptionListComponent } from './option-list/option-list.component';
-import { OptionManageComponent } from './option-manage/option-manage.component';
 
 @NgModule({
   declarations: [
     OptionListComponent,
-    OptionManageComponent,
-    OptionDetailComponent,
   ],
   imports: [
     IdentityConcentratorSharedModule,
