722303dfc2a8dc74ed94b2c896dabe5390601a71
Milan Panik <milan.panik1@gmail.com>
2019-03-21 10:53:01 +0100

    Update ic options forms

--- angular/src/app/admin/identity-concetrator/options/option-list/option-list.component.ts
+++ angular/src/app/admin/identity-concetrator/options/option-list/option-list.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
 
-import { EntityModel } from '../../../shared/entity.model';
 import { OptionService } from '../options.service';
 
 @Component({
@@ -12,14 +9,39 @@ import { OptionService } from '../options.service';
 })
 export class OptionListComponent implements OnInit {
 
-  options$: Observable<any[]>;
+  options: any[];
+  newOptionValue: any = null;
+  error: string = null;
 
-  constructor(private optionService: OptionService) {
+  constructor(public optionService: OptionService) {
   }
 
   ngOnInit() {
-    this.options$ = this.optionService.get()
-      .pipe(map(EntityModel.arrayFromMap));
+    this.optionService.get()
+      .subscribe((options) => this.options = options);
+  }
+
+  updateOptionValue(optionKey, newValue) {
+    const newOptions = this.cloneOptions();
+    newOptions[optionKey] = newValue;
+
+    this.optionService.update(newOptions)
+      .subscribe(
+        () => this.options[optionKey] = newValue,
+        (response) => {
+          const message = response.error;
+          this.error = message.substring(message.lastIndexOf(':') + 1);
+        }
+      );
+  }
+
+  resetSelection() {
+    this.error = null;
+    this.newOptionValue = null;
+  }
+
+  private cloneOptions(): object {
+    return Object.assign({}, this.options);
   }
 
 }
--- angular/src/app/admin/identity-concetrator/options/options.service.ts
+++ angular/src/app/admin/identity-concetrator/options/options.service.ts
@@ -1,17 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { pluck } from 'rxjs/operators';
 
+import { environment } from '../../../../environments/environment';
 import { RealmService } from '../../../realm.service';
-import { IcConfigEntityService } from '../shared/ic-config-entity.service';
+import { HTTP_HEADERS } from '../../../shared/HTTP_HEADERS';
+
+const headers = HTTP_HEADERS;
 
 @Injectable({
   providedIn: 'root',
 })
-export class OptionService extends IcConfigEntityService {
+export class OptionService {
 
-  constructor(http: HttpClient,
-              realmService: RealmService) {
-    super(http, realmService, 'options', 'options');
+  constructor(private http: HttpClient,
+              private realmService: RealmService) {
+
   }
 
+  get(params = {}): Observable<any[]> {
+    return this.realmService.switchMap(realm => {
+      return this.http.get<any>(`${environment.idpApiUrl}/${realm}/config`,
+        { params });
+    }).pipe(
+      pluck('options')
+    );
+  }
+
+  update(newOptions: object): Observable<any> {
+    return this.realmService.switchMap(realm => {
+      return this.http.put(`${environment.idpApiUrl}/${realm}/config/options`,
+        { item: newOptions },
+        { headers }
+      );
+    });
+  }
 }
