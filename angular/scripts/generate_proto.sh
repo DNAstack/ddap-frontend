8afe27d45f785d901cfac502163edc8e58c68bf3
Viliam Rockai <viliam.rockai@gmail.com>
2019-04-03 11:06:59 +0200

    DISCO-2103 Move proto defs to the shared folder

--- angular/scripts/generate_proto.sh
+++ angular/scripts/generate_proto.sh
@@ -8,7 +8,7 @@ OUT_FILE=dam-service
 PROTO_URL="https://raw.githubusercontent.com/cdvoisin/ga4gh-identity/master/dam/api/v1/dam_service.proto"
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 TMP_DIR="./tmp"
-OUT_DIR="./src/app/proto"
+OUT_DIR="./src/app/shared/proto"
 PBJS_BIN="./node_modules/protobufjs/bin/pbjs"
 PBTS_BIN="./node_modules/protobufjs/bin/pbts"
 
