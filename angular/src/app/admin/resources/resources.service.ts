ef6592f5181af821821229bd00812975372224c1
Viliam Rockai <viliam.rockai@gmail.com>
2019-04-03 13:03:25 +0200

    DISCO-2032 Enable resource deletion

--- angular/src/app/admin/resources/resources.service.ts
+++ angular/src/app/admin/resources/resources.service.ts
@@ -4,22 +4,17 @@ import { Observable } from 'rxjs/Observable';
 import { map, pluck } from 'rxjs/operators';
 
 import { environment } from '../../../environments/environment';
-import { HTTP_HEADERS } from '../../shared/HTTP_HEADERS';
 import { realmIdPlaceholder } from '../../shared/realm/realm.constant';
-import { ConfigModel } from '../shared/config.model';
-import { ConfigModificationObject } from '../shared/configModificationObject';
+import { ConfigEntityService } from '../shared/config-entity.service';
 import { EntityModel } from '../shared/entity.model';
-import { EntityService } from '../shared/entity.service';
-
-const headers = HTTP_HEADERS;
 
 @Injectable({
   providedIn: 'root',
 })
-export class ResourceService implements EntityService {
+export class ResourceService extends ConfigEntityService {
 
-  constructor(private http: HttpClient) {
-
+  constructor(http: HttpClient) {
+    super(http, 'resources', 'resources');
   }
 
   getAccessRequestToken(resourceId, viewId): Observable<any[]> {
@@ -30,35 +25,9 @@ export class ResourceService implements EntityService {
       );
   }
 
-  get(): Observable<Map<string, EntityModel>> {
-    return this.http.get<ConfigModel>(`${environment.damApiUrl}/${realmIdPlaceholder}/config`)
-      .pipe(
-        map(config => config.resources),
-        map(EntityModel.objectToMap)
-      );
-  }
-
   getResource(resourceName: string): Observable<EntityModel> {
     return this.get().pipe(
       map(resources => resources.get(resourceName))
     );
-  }
-
-  save(id: string, change: ConfigModificationObject): Observable<any> {
-    return this.http.post(`${environment.damApiUrl}/${realmIdPlaceholder}/config/resources/${id}`,
-      change,
-      {headers}
-    );
-  }
-
-  update(id: string, change: ConfigModificationObject): Observable<any> {
-    return this.http.put(`${environment.damApiUrl}/${realmIdPlaceholder}/config/resources/${id}`,
-      change,
-      {headers}
-    );
-  }
-
-  remove(id: string): Observable<any> {
-    throw new Error('Not yet implemented.');
   }
 }
