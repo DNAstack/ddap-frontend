4147c2d03d5d2dfac3448fc3db402f74bb33901b
Viliam Rockai <viliam.rockai@gmail.com>
2019-04-09 15:11:16 +0200

    DISCO-2116 Fix PR review issues, #128

--- angular/src/app/admin/personas/persona-manage/persona-manage.component.ts
+++ angular/src/app/admin/personas/persona-manage/persona-manage.component.ts
@@ -7,6 +7,7 @@ import { ClaimDefinitionService } from '../../claim-definitions/claim-definition
 import { PassportIssuerService } from '../../passport-issuers/passport-issuerss.service';
 import { ConfigModificationObject } from '../../shared/configModificationObject';
 import { PersonaService } from '../personas.service';
+import TestPersona = dam.v1.TestPersona;
 
 @Component({
   selector: 'ddap-persona-manage',
@@ -15,13 +16,13 @@ import { PersonaService } from '../personas.service';
 })
 export class PersonaManageComponent implements OnInit {
 
-  personaForm = this.fb.group({
+  personaForm = this.formBuilder.group({
     id: ['', [Validators.required, Validators.min(3)]],
-    label: ['', Validators.required],
+    label: [''],
     iss: ['', Validators.required],
     sub: ['', Validators.required],
-    standardClaims: this.fb.array([]),
-    ga4ghClaims: this.fb.array([]),
+    standardClaims: this.formBuilder.array([]),
+    ga4ghClaims: this.formBuilder.array([]),
   });
 
   get standardClaims() {
@@ -32,7 +33,7 @@ export class PersonaManageComponent implements OnInit {
     return this.personaForm.get('ga4ghClaims') as FormArray;
   }
 
-  constructor(private fb: FormBuilder,
+  constructor(private formBuilder: FormBuilder,
               private passportIssuerService: PassportIssuerService,
               private claimDefinitionService: ClaimDefinitionService,
               private personaService: PersonaService,
@@ -50,7 +51,7 @@ export class PersonaManageComponent implements OnInit {
   }
 
   addGa4ghClaims() {
-    this.ga4ghClaims.insert(0, this.fb.group({
+    this.ga4ghClaims.insert(0, this.formBuilder.group({
       claimName: ['', Validators.required],
       source: ['', Validators.required],
       value: ['', Validators.required],
@@ -62,7 +63,7 @@ export class PersonaManageComponent implements OnInit {
 
   save() {
     const id = this.personaForm.value.id;
-    const testPersona: dam.v1.TestPersona = dam.v1.TestPersona.create({
+    const testPersona: TestPersona = TestPersona.create({
       idToken: {
         standardClaims: {
           iss: this.personaForm.value.iss,
