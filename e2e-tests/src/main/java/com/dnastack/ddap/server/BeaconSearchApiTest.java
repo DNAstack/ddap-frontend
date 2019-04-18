841685beb1c649774275aa1e742cf91c786fbd4b
Max Barkley <max@dnastack.com>
2019-04-18 18:00:06 -0400

    Revert "DISCO-2104 Add proto file" to fix build

--- e2e-tests/src/main/java/com/dnastack/ddap/server/BeaconSearchApiTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/server/BeaconSearchApiTest.java
@@ -8,8 +8,6 @@ import static org.hamcrest.Matchers.*;
 
 import com.dnastack.ddap.common.AbstractBaseE2eTest;
 import java.io.IOException;
-
-import dam.v1.DamService;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,10 +19,6 @@ public class BeaconSearchApiTest extends AbstractBaseE2eTest {
     @Before
     public void setupRealm() throws IOException {
         String realmConfigString = loadTemplate("/com/dnastack/ddap/aggregateSearchRealmConfig.json");
-
-        DamService.DamConfig.Builder damConfigBuilder = DamService.DamConfig.newBuilder();
-        validateProtoBuf(realmConfigString, damConfigBuilder);
-
         setupRealmConfig("nci_researcher", realmConfigString, REALM);
     }
 
--- generateProtobufObjects.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-#Helper script to generate Java protobuf definition.
-
-SRC_DIR="./angular/proto"
-DST_DIR="./e2e-tests/src/main/java"
-time protoc --java_out=$DST_DIR $SRC_DIR/dam_service.proto
