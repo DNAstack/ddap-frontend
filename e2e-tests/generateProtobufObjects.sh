9dfa97be03cc91c32b81da3e9e37d463c41f2384
Harsh Dwivedi <harsh@dnastack.com>
2019-04-18 17:08:23 -0400

    Add proto file.

--- e2e-tests/generateProtobufObjects.sh
+++ e2e-tests/generateProtobufObjects.sh
@@ -4,6 +4,9 @@ set -x
 
 #Helper script to generate Java protobuf definition.
 
-SRC_DIR="./src/main/resources/protos"
+
+/Users/harshd/WebstormProjects/ddap-frontend/angular/proto/dam_service.proto
+
+SRC_DIR="/Users/harshd/WebstormProjects/ddap-frontend/angular/proto"
 DST_DIR="./src/main/java"
-time protoc --java_out=$DST_DIR $SRC_DIR/dam_service.proto
+time protoc --java_out=$DST_DIR -I $SRC_DIR $SRC_DIR/dam_service.proto
