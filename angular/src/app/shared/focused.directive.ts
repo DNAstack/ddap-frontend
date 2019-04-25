baa0d2a477c25f0d1d04c92b01d4e2101831a352
Viliam Rockai <viliam.rockai@gmail.com>
2019-04-25 12:27:15 +0200

    DISCO-2162 On-demand autocomplete fix

--- angular/src/app/shared/focused.directive.ts
+++ angular/src/app/shared/focused.directive.ts
@@ -3,17 +3,19 @@ import { Directive, HostListener } from '@angular/core';
 @Directive({
   // tslint:disable-next-line
   selector: '[matAutocomplete]',
-  exportAs: 'isFocused',
+  exportAs: 'ddapInputState',
 })
 export class FocusedDirective {
 
   isFocused = false;
+  isTouched = false;
 
   constructor() {
 
   }
 
   @HostListener('focus') onFocus() {
+    this.isTouched = true;
     this.isFocused = true;
   }
 
