960f87dad930b23608b18a399c0bdd296b1e7b9e
Milan Panik <milan.panik1@gmail.com>
2019-04-28 22:33:33 +0200

    DISCO-2178 add constraints to realm name input

--- angular/src/app/layout/layout.component.ts
+++ angular/src/app/layout/layout.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import _get from 'lodash/get';
 
 import { IdentityService } from '../identity/identity.service';
 import { Profile } from '../identity/profile.model';
-import {
-  RealmChangeConfirmationDialogComponent
-} from '../shared/realm-change-confirmation-dialog/realm-change-confirmation-dialog.component';
 
 @Component({
   templateUrl: './layout.component.html',
@@ -18,11 +14,9 @@ export class LayoutComponent implements OnInit {
 
   profile: Profile = null;
   realm: string;
-  realmInputValue: string;
   loginPath: string;
 
   constructor(public loader: LoadingBarService,
-              public dialog: MatDialog,
               private router: Router,
               private activatedRoute: ActivatedRoute,
               private identityService: IdentityService) {
@@ -35,7 +29,7 @@ export class LayoutComponent implements OnInit {
     });
 
     this.activatedRoute.root.firstChild.params.subscribe((params) => {
-      this.realm = this.realmInputValue = params.realmId;
+      this.realm = params.realmId;
       this.loginPath = `/api/v1alpha/${this.realm}/identity/login`;
     });
   }
@@ -50,17 +44,6 @@ export class LayoutComponent implements OnInit {
   isIdentityPage() {
     const currentRoute = _get(this.activatedRoute, 'snapshot.firstChild.url[0].path');
     return currentRoute === 'identity';
-  }
-
-  openRealmChangeConfirmationDialog(realm): void {
-    const dialogRef = this.dialog.open(RealmChangeConfirmationDialogComponent, {
-      data: { realm },
-    });
-    dialogRef.afterClosed().subscribe(acknowledged => {
-      if (!acknowledged) {
-        this.realmInputValue = this.realm;
-      }
-    });
   }
 
 }
