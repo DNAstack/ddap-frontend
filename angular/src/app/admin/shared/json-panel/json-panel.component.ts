80ca1edce0e9f7cff338a0cf2f750886b051fcde
Viliam Rockai <vrockai@users.noreply.github.com>
2019-04-29 18:39:37 +0200

    DISCO-2176 form for resource test editing (#164)
    * DISCO-2176 form for resource test editing
    
    * DISCO-2176 Make test form look good
    
    * DISCO-2176 only use resources access in the test form

--- angular/src/app/admin/shared/json-panel/json-panel.component.ts
+++ angular/src/app/admin/shared/json-panel/json-panel.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
-import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/internal/observable/of';
+import { catchError, debounceTime, switchMap, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
 
+import { ResourceService } from '../../resources/resources.service';
 import { ConfigModificationObject } from '../configModificationObject';
 import { EntityModel } from '../entity.model';
 import { EntityService } from '../entity.service';
 import { JsonEditorDefaults } from '../jsonEditorDefaults';
+import { TestFormComponent } from '../test-form/test-form.component';
 
 enum ViewState {
   Editing,
@@ -19,7 +23,7 @@ enum ViewState {
   templateUrl: './json-panel.component.html',
   styleUrls: ['./json-panel.component.scss'],
 })
-export class JsonPanelComponent implements OnChanges {
+export class JsonPanelComponent implements OnChanges, OnDestroy {
   error: any = null;
 
   @Input()
@@ -42,12 +46,21 @@ export class JsonPanelComponent implements OnChanges {
   @ViewChild('errorEditor')
   errorEditor: JsonEditorComponent;
 
+  @ViewChild(TestFormComponent)
+  testForm: TestFormComponent;
+
   entityEditorOptions: JsonEditorOptions;
   testEditorOptions: JsonEditorOptions;
   errorEditorOptions: JsonEditorOptions;
 
   @Input()
   entityService: EntityService;
+
+  isResource = false;
+
+  save$: Subject<any> = new Subject();
+
+  private saveSubscription;
 
   constructor(private router: Router) {
     this.entityEditorOptions = new JsonEditorDefaults();
@@ -56,36 +69,85 @@ export class JsonPanelComponent implements OnChanges {
     this.testEditorOptions.mode = 'code';
     this.errorEditorOptions.mode = 'code';
     this.errorEditorOptions.onEditable = () => false;
+
+    this.saveSubscription = this.save$.pipe(
+      debounceTime(800),
+      switchMap((dryRun) => this.saveResource(dryRun))
+    ).subscribe();
   }
 
   ngOnChanges () {
     this.entityDto = (this.entity != null) ? this.entity.dto : null;
+    this.isResource = this.entityService instanceof ResourceService;
+  }
+
+  ngOnDestroy(): void {
+    this.saveSubscription.unsubscribe();
   }
 
   updateResourceDto(event: any) {
-    this.entityDto = event;
+    if (this.entityEditor.isValidJson()) {
+      this.entityDto = event;
+
+      if (this.isResource) {
+        this.save$.next(true);
+      }
+    }
   }
 
   updateTestDto(event: any) {
     this.testDto = event;
   }
 
+  validateTest() {
+    this.save$.next(true);
+  }
+
   save() {
+    this.save$.next(false);
+  }
+
+  saveResource(isDryRun = false) {
     this.state = ViewState.Submitting;
-    let updateAction: Observable<any>;
 
-    const change = new ConfigModificationObject(this.entityDto, this.testDto);
-    updateAction = this.entityService.update(this.entity.name, change);
+    const testDto = this.getTestDto();
 
-    updateAction
-      .subscribe(() => {
-        this.state = ViewState.Viewing;
-        this.setEditorMode('view');
-        this.error = null;
-      }, e => {
+    if (isDryRun) {
+      testDto['dry_run'] = true;
+    }
+
+    const change = new ConfigModificationObject(this.entityDto, testDto);
+
+    return this.entityService.update(this.entity.name, change).pipe(
+      catchError((e) => {
         this.state = ViewState.Editing;
         this.error = e.error;
-      });
+
+        if (this.isResource) {
+          this.testForm.validatePersonaFields(e);
+        }
+
+        return of(true);
+      }),
+      tap((wasError) => {
+        if (wasError) {
+          return;
+        }
+
+        this.error = null;
+
+        if (isDryRun) {
+          this.state = ViewState.Editing;
+
+          if (this.isResource) {
+            return this.testForm.makeFieldsValid();
+          }
+        }
+
+        this.state = ViewState.Viewing;
+        this.setEditorMode('view');
+      })
+    );
   }
 
   cancel() {
@@ -109,6 +171,14 @@ export class JsonPanelComponent implements OnChanges {
         this.state = ViewState.Viewing;
         this.error = e.error;
       });
+  }
+
+  private getTestDto() {
+    if (!this.isResource) {
+      return this.testDto;
+    }
+
+    return this.testForm.toApplyDto();
   }
 
   private setEditorMode(mode) {
--- angular/src/app/admin/shared/shared.module.ts
+++ angular/src/app/admin/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MAT_CHECKBOX_CLICK_ACTION, MatButtonToggleModule } from '@angular/material';
 import { NgJsonEditorModule } from 'ang-jsoneditor';
 
 import { SharedModule } from '../../shared/shared.module';
@@ -8,6 +9,7 @@ import { EntityListComponent } from './entity-list/entity-list.component';
 import { EntityManageFormComponent } from './entity-manage-form/entity-manage-form.component';
 import { JsonPanelComponent } from './json-panel/json-panel.component';
 import { OptionEditableListComponent } from './option-editable-list/option-editable-list.component';
+import { TestFormComponent } from './test-form/test-form.component';
 
 @NgModule({
   declarations: [
@@ -16,10 +18,12 @@ import { OptionEditableListComponent } from './option-editable-list/option-edita
     EntityAddComponent,
     EntityManageFormComponent,
     OptionEditableListComponent,
+    TestFormComponent,
   ],
   imports: [
     SharedModule,
     NgJsonEditorModule,
+    MatButtonToggleModule,
   ],
   exports: [
     SharedModule,
@@ -29,6 +33,9 @@ import { OptionEditableListComponent } from './option-editable-list/option-edita
     EntityAddComponent,
     EntityManageFormComponent,
     OptionEditableListComponent,
+  ],
+  providers: [
+    {provide: MAT_CHECKBOX_CLICK_ACTION, useValue: 'check-indeterminate'},
   ],
 })
 export class AdminSharedModule { }
