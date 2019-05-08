c5d24d2d2f82420cdd3f5cad16d13a476141fa5c
Viliam Rockai <viliam.rockai@gmail.com>
2019-05-08 12:10:40 +0200

    DISCO-2094 Pre-fetch resource data

--- angular/src/app/shared/realm/realm-interceptor.service.ts
+++ angular/src/app/shared/realm/realm-interceptor.service.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import _get from 'lodash.get';
 import { Observable } from 'rxjs/Observable';
 
 import { realmIdPlaceholder } from './realm.constant';
@@ -13,7 +14,8 @@ export class RealmInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const realmId = this.activatedRoute.root.firstChild.snapshot.params.realmId;
+    const realmId = _get(this.activatedRoute, 'root.firstChild.snapshot.params.realmId');
+
     const secureReq = req.clone({
       url: req.url.replace(realmIdPlaceholder, realmId),
     });
