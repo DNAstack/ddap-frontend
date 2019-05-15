0a86054da3b479fd92935629db20db315cd995df
Viliam Rockai <viliam.rockai@gmail.com>
2019-05-15 18:05:19 +0200

    DISCO-2233 Trusted Sources form introduction

--- angular/src/app/admin/trusted-sources/trusted-sources-manage/trusted-sources-manage.component.ts
+++ angular/src/app/admin/trusted-sources/trusted-sources-manage/trusted-sources-manage.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { ConfigModificationObject } from '../../shared/configModificationObject';
+import { EntityModel } from '../../shared/entity.model';
+import { TrustedSourcesFormComponent } from '../trusted-sources-form/trusted-sources-form.component';
 import { TrustedSourcesService } from '../trusted-sources.service';
 
 @Component({
@@ -9,13 +12,20 @@ import { TrustedSourcesService } from '../trusted-sources.service';
   styleUrls: ['./trusted-sources-manage.component.scss'],
 })
 export class TrustedSourcesManageComponent {
+
+  @ViewChild(TrustedSourcesFormComponent)
+  trustedSourcesForm: TrustedSourcesFormComponent;
+
   constructor(private trustedSourcesService: TrustedSourcesService,
               private router: Router,
               private route: ActivatedRoute) {
   }
 
-  save(id, change) {
-    this.trustedSourcesService.save(id, change)
+  save() {
+    const trustedSourcesModel: EntityModel = this.trustedSourcesForm.getModel();
+    const change = new ConfigModificationObject(trustedSourcesModel.dto, {});
+
+    this.trustedSourcesService.save(trustedSourcesModel.name, change)
       .subscribe(() => this.router.navigate(['../..'], { relativeTo: this.route }));
   }
 }
