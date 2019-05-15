e745cd0d76c10601075d37d33902afb7cce3c656
Milan Panik <milan.panik1@gmail.com>
2019-05-15 10:37:54 +0200

    DISCO-2229 Add DAM client application form

--- angular/src/app/admin/client-applications/client-application-detail/client-application-detail.component.ts
+++ angular/src/app/admin/client-applications/client-application-detail/client-application-detail.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
+import { FormValidationService } from '../../../shared/form-validation.service';
+import { ConfigModificationObject } from '../../shared/configModificationObject';
 import { EntityDetailBase } from '../../shared/entity-detail.base';
+import { EntityModel } from '../../shared/entity.model';
+import { ClientApplicationFormComponent } from '../client-application-form/client-application-form.component';
 import { ClientApplicationService } from '../client-applications.service';
 
 @Component({
@@ -11,11 +15,33 @@ import { ClientApplicationService } from '../client-applications.service';
 })
 export class ClientApplicationDetailComponent extends EntityDetailBase<ClientApplicationService> {
 
-  constructor(
-    route: ActivatedRoute,
-    service: ClientApplicationService
-  ) {
+  @ViewChild(ClientApplicationFormComponent)
+  clientApplicationForm: ClientApplicationFormComponent;
+
+  constructor(route: ActivatedRoute,
+              service: ClientApplicationService,
+              private router: Router,
+              private formValidation: FormValidationService) {
     super(route, service, 'clientName');
   }
+
+  update() {
+    if (!this.clientApplicationForm.form.valid) {
+      this.formValidation.forceValidate(this.clientApplicationForm.form);
+      return;
+    }
+
+    const clientApplication: EntityModel = this.clientApplicationForm.getModel();
+    const change = new ConfigModificationObject(clientApplication.dto, {});
+    this.entityService.update(this.entity.name, change)
+      .subscribe(this.navigateUp);
+  }
+
+  delete() {
+    this.entityService.remove(this.entity.name)
+      .subscribe(this.navigateUp);
+  }
+
+  private navigateUp = () => this.router.navigate(['..'], { relativeTo: this.route });
 
 }
