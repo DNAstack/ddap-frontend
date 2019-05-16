06eb2d5e713facceb39192c36522c7b86ba29148
Max Barkley <max@dnastack.com>
2019-05-15 21:23:24 -0400

    Add BFF endpoint for resolving target adapter variables for a service template.

---
+++ e2e-tests/src/main/java/com/dnastack/ddap/server/VariableSuggestionApiTest.java
@@ -0,0 +1,74 @@
+package com.dnastack.ddap.server;
+
+import com.dnastack.ddap.common.AbstractBaseE2eTest;
+import dam.v1.e2e.DamService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+
+public class VariableSuggestionApiTest extends AbstractBaseE2eTest {
+
+    private static final String REALM = generateRealmName(VariableSuggestionApiTest.class.getSimpleName());
+
+    @Before
+    public void setupRealm() throws IOException {
+        String configJson = loadTemplate("/com/dnastack/ddap/variableSuggestionConfig.json");
+        DamService.DamConfig.Builder damConfigBuilder = DamService.DamConfig.newBuilder();
+        validateProtoBuf(configJson, damConfigBuilder);
+        setupRealmConfig("administrator", configJson, REALM);
+    }
+
+    @Test
+    public void shouldFindBigQueryVariables() throws IOException {
+        String validPersonaToken = fetchRealPersonaDamToken("administrator", REALM);
+
+        /* Run the aggregate search query on the realm */
+        // @formatter:off
+        given()
+                    .log().method()
+                    .log().uri()
+                    .when()
+                    .auth().basic(DDAP_USERNAME, DDAP_PASSWORD)
+                    .cookie("dam_token", validPersonaToken)
+                    .get("/api/v1alpha/" + REALM + "/serviceTemplates/variables?serviceTemplate=bigquery")
+                    .then()
+                    .log().ifValidationFails()
+                    .contentType(JSON)
+                    .statusCode(200)
+                    .body("project", notNullValue())
+                    .body("project.regexp", notNullValue(String.class))
+                    .body("table", notNullValue())
+                    .body("table.regexp", notNullValue(String.class));
+        // @formatter:on
+    }
+
+    @Test
+    public void shouldFindGCSVariables() throws IOException {
+        String validPersonaToken = fetchRealPersonaDamToken("administrator", REALM);
+
+        /* Run the aggregate search query on the realm */
+        // @formatter:off
+        given()
+                .log().method()
+                .log().uri()
+                .when()
+                .auth().basic(DDAP_USERNAME, DDAP_PASSWORD)
+                .cookie("dam_token", validPersonaToken)
+                .get("/api/v1alpha/" + REALM + "/serviceTemplates/variables?serviceTemplate=gcs")
+                .then()
+                .log().ifValidationFails()
+                .contentType(JSON)
+                .statusCode(200)
+                .body("project", notNullValue())
+                .body("project.regexp", notNullValue(String.class))
+                .body("bucket", notNullValue())
+                .body("bucket.regexp", notNullValue(String.class));
+        // @formatter:on
+    }
+}
---
+++ src/main/java/com/dnastack/ddapfrontend/route/ServiceTemplateVariableResolutionController.java
@@ -0,0 +1,86 @@
+package com.dnastack.ddapfrontend.route;
+
+import com.dnastack.ddapfrontend.client.dam.DamClient;
+import com.dnastack.ddapfrontend.security.UserTokenCookiePackager;
+import dam.v1.DamService;
+import dam.v1.DamService.DamConfig;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1alpha/{realm}/serviceTemplates/variables")
+public class ServiceTemplateVariableResolutionController {
+
+    @Autowired
+    private DamClient damClient;
+
+    @Autowired
+    private UserTokenCookiePackager cookiePackager;
+
+    @GetMapping
+    public Map<String, VariableFormat> resolveVariables(@RequestParam(name = "serviceTemplate") String serviceTemplateId, ServerHttpRequest request, @PathVariable String realm) {
+        Optional<String> foundDamToken = cookiePackager.extractToken(request, UserTokenCookiePackager.CookieKind.DAM);
+        String damToken = foundDamToken.orElseThrow(() -> new IllegalArgumentException("Authorization dam token is required."));
+        DamConfig damConfig = damClient.getConfig(damToken, realm);
+        final DamService.ServiceTemplate foundServiceTemplate = damConfig.getServiceTemplatesMap().get(serviceTemplateId);
+
+        if (foundServiceTemplate == null) {
+            throw new IllegalArgumentException(format("Unrecognized serviceTemplate id [%s]", serviceTemplateId));
+        } else {
+            final String targetAdapterId = foundServiceTemplate.getTargetAdapter();
+            final String itemFormatId = foundServiceTemplate.getItemFormat();
+
+            final DamService.TargetAdaptersResponse targetAdapters = damClient.getTargetAdapters(damToken, realm);
+            final DamService.TargetAdapter targetAdapter = targetAdapters.getTargetAdaptersMap().get(targetAdapterId);
+            final DamService.ItemFormat itemFormat = Optional.ofNullable(targetAdapter)
+                                                             .map(ta -> ta.getItemFormatsMap().get(itemFormatId))
+                                                             .orElse(null);
+
+            if (targetAdapter == null) {
+                throw new IllegalStateException(format(
+                        "Could not find targetAdapter [%s] referenced from service template [%s]",
+                        targetAdapterId,
+                        serviceTemplateId));
+            } else if (itemFormat == null) {
+                throw new IllegalStateException(format(
+                        "Could not find itemFormat [%s] in targetAdapter [%s] referenced from service template [%s]",
+                        itemFormatId,
+                        targetAdapterId,
+                        serviceTemplateId));
+            } else {
+                return itemFormat.getVariablesMap()
+                                 .entrySet()
+                                 .stream()
+                                 .collect(Collectors.toMap(Map.Entry::getKey, e -> variableFormatToMap(e.getValue())));
+            }
+        }
+    }
+
+    private VariableFormat variableFormatToMap(DamService.VariableFormat variableFormat) {
+        return new VariableFormat(variableFormat.getRegexp(), variableFormat.getOptional(), variableFormat.getUiMap());
+    }
+
+    /**
+     * This type only exists because I could not configure the Spring MVC serializer to properly serialize
+     * the protobuf VariableFormat definition.
+     */
+    @Data
+    @AllArgsConstructor
+    static class VariableFormat {
+        private String regexp;
+        private boolean optional;
+        private Map<String, String> ui;
+    }
+
+}
