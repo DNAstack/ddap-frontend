a6b3748c2f9bcfe73ee8dd0439be9ce5514af44b
Viliam Rockai <viliam.rockai@gmail.com>
2019-05-20 14:59:16 +0200

    DISCO-2232 fix filename typo

--- angular/src/app/admin/passport-issuers/passport-issuer-manage/passport-issuer-manage.component.ts
+++ angular/src/app/admin/passport-issuers/passport-issuer-manage/passport-issuer-manage.component.ts
@@ -6,7 +6,7 @@ import { ConfigModificationObject } from '../../shared/configModificationObject'
 import { EntityModel } from '../../shared/entity.model';
 import { FormErrorScrollService } from '../../shared/form-error-scroll.service';
 import { PassportIssuerFormComponent } from '../passport-issuer-form/passport-issuer-form.component';
-import { PassportIssuerService } from '../passport-issuerss.service';
+import { PassportIssuerService } from '../passport-issuers.service';
 import TrustedPassportIssuer = dam.v1.TrustedPassportIssuer;
 
 @Component({
