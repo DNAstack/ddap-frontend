f53dac9c821532a932877f284dfebfa710335a28
Milan Panik <milan.panik1@gmail.com>
2019-05-20 16:25:23 +0200

    DISCO-2119 remove leftover comment

--- angular/src/app/admin/resources/resources.service.ts
+++ angular/src/app/admin/resources/resources.service.ts
@@ -11,7 +11,6 @@ import { realmIdPlaceholder } from '../../shared/realm/realm.constant';
 import { ConfigEntityService } from '../shared/config-entity.service';
 import GetTokenResponse = dam.v1.GetTokenResponse;
 import IGetTokenRequest = dam.v1.IGetTokenRequest;
-import { ConfigModel } from '../shared/config.model';
 import { EntityModel } from '../shared/entity.model';
 
 @Injectable({
@@ -31,18 +30,6 @@ export class ResourceService extends ConfigEntityService {
         map(resources => resources.get(resourceName))
       );
   }
-
-  // // TODO: dataService dups
-  // getResource(resourceId: string, realmId = null, params = {}): Observable<EntityModel> {
-  //   return this.http.get<any>(
-  //     `${environment.damApiUrl}/${realmId || realmIdPlaceholder}/resources/${resourceId}`,
-  //     {params}
-  //   ).pipe(
-  //     this.errorHandler.notifyOnError(`Can't load resource ${resourceId}.`),
-  //     pluck('resource'),
-  //     map((resource) => new EntityModel(resourceId, resource))
-  //   );
-  // }
 
   getAccessRequestToken(resourceId: string, viewId: string, tokenRequest: IGetTokenRequest): Observable<GetTokenResponse> {
     return this.http.get<GetTokenResponse>(
