114ba09fd5a5819ff38c9ce6a5996affef5cd646
Viliam Rockai <viliam.rockai@gmail.com>
2019-05-21 15:13:08 +0200

    DISCO-2251 Include IC definitions

--- angular/scripts/generate_proto.sh
+++ angular/scripts/generate_proto.sh
@@ -3,18 +3,28 @@
 set -e
 set -x
 
-SRC_PROTO_FILE=./proto/dam_service.proto
-OUT_FILE=dam-service
+SRC_DAM_PROTO_FILE=./proto/dam_service.proto
+SRC_IC_PROTO_FILE=./proto/ic_service.proto
+OUT_DAM_FILE=dam-service
+OUT_IC_FILE=ic-service
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 TMP_DIR="./tmp"
-TMP_PROTO_FILE=${TMP_DIR}/dam_service.proto
+TMP_PROTO_FILE=${TMP_DIR}/temporary.proto
 OUT_DIR="./src/app/shared/proto"
 PBJS_BIN="./node_modules/protobufjs/bin/pbjs"
 PBTS_BIN="./node_modules/protobufjs/bin/pbts"
 
 mkdir -p ${TMP_DIR}
 mkdir -p ${OUT_DIR}
-cp ${SRC_PROTO_FILE} ${TMP_PROTO_FILE}
 
-${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_FILE}.js ${TMP_PROTO_FILE}
-${PBTS_BIN} -o ${OUT_DIR}/${OUT_FILE}.d.ts ${OUT_DIR}/${OUT_FILE}.js
+# DAM
+cp ${SRC_DAM_PROTO_FILE} ${TMP_PROTO_FILE}
+
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_DAM_FILE}.js ${TMP_PROTO_FILE}
+${PBTS_BIN} -o ${OUT_DIR}/${OUT_DAM_FILE}.d.ts ${OUT_DIR}/${OUT_DAM_FILE}.js
+
+# IC
+cp ${SRC_IC_PROTO_FILE} ${TMP_PROTO_FILE}
+
+${PBJS_BIN} -t static-module -w commonjs -o ${OUT_DIR}/${OUT_IC_FILE}.js ${TMP_PROTO_FILE}
+${PBTS_BIN} -o ${OUT_DIR}/${OUT_IC_FILE}.d.ts ${OUT_DIR}/${OUT_IC_FILE}.js
