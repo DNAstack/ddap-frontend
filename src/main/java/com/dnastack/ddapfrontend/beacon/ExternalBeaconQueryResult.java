8aca128b9fcd407615de85001f093a083cea4cef
Harsh Dwivedi <harsh@dnastack.com>
2019-05-21 18:03:09 -0400

    Revert "Add beacon info to response (#182)"
    This reverts commit 706e3ea13e484c4bdb50c3057840f48b8ef10911.

--- src/main/java/com/dnastack/ddapfrontend/beacon/ExternalBeaconQueryResult.java
+++ src/main/java/com/dnastack/ddapfrontend/beacon/ExternalBeaconQueryResult.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,12 +11,12 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExternalBeaconQueryResult {
-    @Nullable
-    private BeaconInfo beaconInfo;
-    private String uiLabel;
+
     private Map<String, String> resource = new HashMap<>();
+    private String name, organization;
     private String error;
     private Boolean exists;
     private Map<String, Object> metadata = new HashMap<>();
     private Map<String, String> info = new HashMap<>();
+
 }
