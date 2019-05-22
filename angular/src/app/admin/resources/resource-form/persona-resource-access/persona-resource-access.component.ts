4cde4822ccf53410c58b679beb7977725ed03d93
Milan Panik <milan.panik1@gmail.com>
2019-05-22 10:28:37 +0200

    DISCO-2119 Add checkboxes for persona access

---
+++ angular/src/app/admin/resources/resource-form/persona-resource-access/persona-resource-access.component.ts
@@ -0,0 +1,79 @@
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import _get from 'lodash.get';
+import { of } from 'rxjs/internal/observable/of';
+import { catchError, debounceTime, switchMap, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
+
+import { ConfigModificationObject } from '../../../shared/configModificationObject';
+import { EntityModel } from '../../../shared/entity.model';
+import { TestFormComponent } from '../../../shared/test-form/test-form.component';
+import { ResourceService } from '../../resources.service';
+
+@Component({
+  selector: 'ddap-persona-resource-access',
+  templateUrl: './persona-resource-access.component.html',
+  styleUrls: ['./persona-resource-access.component.scss'],
+})
+export class PersonaResourceAccessComponent {
+
+  @Input()
+  resource: EntityModel;
+  @ViewChild(TestFormComponent)
+  testForm: TestFormComponent;
+
+  error: any = null;
+  save$: Subject<any> = new Subject();
+
+  private saveSubscription;
+
+  constructor(private resourceService: ResourceService) {
+    this.saveSubscription = this.save$.pipe(
+      debounceTime(800),
+      switchMap(({changes, isDryRun}) => this.saveResource(changes, isDryRun))
+    ).subscribe();
+  }
+
+  getApplyModel() {
+    return this.testForm.toApplyDto();
+  }
+
+  dryRun(changes?: EntityModel) {
+    if (changes) {
+      this.resource = changes;
+    }
+    this.save$.next({ changes, isDryRun: true });
+  }
+
+  save(changes: EntityModel) {
+    this.save$.next({ changes, isDryRun: false });
+  }
+
+  private saveResource(changes: object, isDryRun = true) {
+    const applyModel = this.getApplyModel();
+
+    if (isDryRun) {
+      applyModel['dry_run'] = true;
+    }
+
+    const change = new ConfigModificationObject(_get(changes, 'dto', this.resource.dto), applyModel);
+
+    return this.resourceService.update(this.resource.name, change).pipe(
+      catchError((e) => {
+        this.error = e.error;
+        this.testForm.validatePersonaFields(e);
+
+        return of(true);
+      }),
+      tap((wasError) => {
+        if (wasError) {
+          return;
+        }
+        this.error = null;
+
+        if (isDryRun) {
+          return this.testForm.makeFieldsValid();
+        }
+      })
+    );
+  }
+}
--- angular/src/app/admin/resources/resources.module.ts
+++ angular/src/app/admin/resources/resources.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AdminSharedModule } from '../shared/shared.module';
 
 import { ResourceDetailComponent } from './resource-detail/resource-detail.component';
+import { PersonaResourceAccessComponent } from './resource-form/persona-resource-access/persona-resource-access.component';
 import { ResourceFormComponent } from './resource-form/resource-form.component';
 import { ResourceViewFormComponent } from './resource-form/resource-view-form/resource-view-form.component';
 import { ResourceListComponent } from './resource-list/resource-list.component';
@@ -15,6 +16,7 @@ import { ResourceManageComponent } from './resource-manage/resource-manage.compo
     ResourceDetailComponent,
     ResourceFormComponent,
     ResourceViewFormComponent,
+    PersonaResourceAccessComponent,
   ],
   imports: [
     AdminSharedModule,
