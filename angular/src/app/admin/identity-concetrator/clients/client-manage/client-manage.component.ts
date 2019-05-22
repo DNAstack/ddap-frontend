906a8f677637517a1eb29b46eabf2377fb217c9a
Viliam Rockai <viliam.rockai@gmail.com>
2019-05-22 15:53:09 +0200

    DISCO-2228 Add clients form

--- angular/src/app/admin/identity-concetrator/clients/client-manage/client-manage.component.ts
+++ angular/src/app/admin/identity-concetrator/clients/client-manage/client-manage.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { ClientService } from '../clients.service';
+import { ConfigModificationObject } from '../../../shared/configModificationObject';
+import { EntityModel } from '../../../shared/entity.model';
+import { FormErrorScrollService } from '../../../shared/form-error-scroll.service';
+import { TrustedSourcesFormComponent } from '../../../trusted-sources/trusted-sources-form/trusted-sources-form.component';
+import { TrustedSourcesService } from '../../../trusted-sources/trusted-sources.service';
 
 @Component({
   selector: 'ddap-client-manage',
@@ -10,13 +14,29 @@ import { ClientService } from '../clients.service';
 })
 export class ClientManageComponent {
 
-  constructor(private clientService: ClientService,
+  @ViewChild(TrustedSourcesFormComponent)
+  trustedSourcesForm: TrustedSourcesFormComponent;
+
+  @ViewChild('formMatError')
+  formErrorElement: ElementRef;
+
+  submitted = false;
+
+  constructor(private trustedSourcesService: TrustedSourcesService,
               private router: Router,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              public formError: FormErrorScrollService) {
   }
 
-  save(id, change) {
-    this.clientService.save(id, change)
+  save() {
+    const trustedSourcesModel: EntityModel = this.trustedSourcesForm.getModel();
+    const change = new ConfigModificationObject(trustedSourcesModel.dto, {});
+
+    if (!this.formError.validate(this.trustedSourcesForm.form, this.formErrorElement)) {
+      return;
+    }
+
+    this.trustedSourcesService.save(trustedSourcesModel.name, change)
       .subscribe(() => this.router.navigate(['../..'], { relativeTo: this.route }));
   }
 }
