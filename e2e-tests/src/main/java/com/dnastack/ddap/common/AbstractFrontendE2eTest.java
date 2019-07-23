01c9327076f1bb3d6409390134afbade1667caf9
Milan Panik <milan.panik1@gmail.com>
2019-07-23 08:42:53 +0200

    Remove avoidAutoVersion() from chrome driver

--- e2e-tests/src/main/java/com/dnastack/ddap/common/AbstractFrontendE2eTest.java
+++ e2e-tests/src/main/java/com/dnastack/ddap/common/AbstractFrontendE2eTest.java
@@ -35,7 +35,6 @@ public abstract class AbstractFrontendE2eTest extends AbstractBaseE2eTest {
     public static void driverSetup() {
         screenshotDir = optionalEnv("E2E_SCREENSHOT_DIR", "target");
         WebDriverManager.chromedriver()
-                .avoidAutoVersion()
                 .setup();
         ChromeOptions options = new ChromeOptions();
         if (HEADLESS) {
