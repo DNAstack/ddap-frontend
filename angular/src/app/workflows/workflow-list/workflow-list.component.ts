be152762bd3d1a7c572c66ea0ffba524e1f5fb93
Milan Panik <milan.panik1@gmail.com>
2019-09-11 14:16:59 +0200

    Add option to refresh data on list page after creation

--- angular/src/app/workflows/workflow-list/workflow-list.component.ts
+++ angular/src/app/workflows/workflow-list/workflow-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { Workflow } from '../workflow.model';
@@ -14,18 +16,47 @@ export class WorkflowListComponent implements OnInit, OnDestroy {
   workflows: Workflow[];
   workflowSubscription: Subscription;
 
-  constructor(private workflowService: WorkflowService) {
+  newlyCreatedWorkflowId?: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private snackBar: MatSnackBar,
+              private workflowService: WorkflowService) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      this.newlyCreatedWorkflowId = navigation.extras.state.runId;
+    }
   }
 
   ngOnInit(): void {
+    this.getWorkflows();
+    if (this.newlyCreatedWorkflowId) {
+      this.notifyAboutNewWorkflow();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.workflowSubscription.unsubscribe();
+  }
+
+  private getWorkflows() {
+    if (this.workflowSubscription) {
+      this.workflowSubscription.unsubscribe();
+    }
     this.workflowSubscription = this.workflowService.getAllWorkflowRuns()
       .subscribe((workflows: Workflow[]) => {
         this.workflows = workflows;
       });
   }
 
-  ngOnDestroy(): void {
-    this.workflowSubscription.unsubscribe();
+  private notifyAboutNewWorkflow() {
+    const message = `Your workflow ID is ${this.newlyCreatedWorkflowId}. If you can't see it refresh the data.`;
+    const snackBarRef = this.snackBar.open(message, 'Refresh', {
+      duration: 8000,
+    });
+    snackBarRef.onAction().subscribe(() => {
+      this.getWorkflows();
+    });
   }
 
 }
