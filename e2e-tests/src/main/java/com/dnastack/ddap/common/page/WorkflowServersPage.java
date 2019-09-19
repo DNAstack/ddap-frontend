0f8e6c3fb174783c35f7f71f17316916017832e3
Monica <monica@dnastack.com>
2019-09-19 18:27:56 -0400

    updated e2e tests

---
+++ e2e-tests/src/main/java/com/dnastack/ddap/common/page/WorkflowServersPage.java
@@ -0,0 +1,32 @@
+package com.dnastack.ddap.common.page;
+
+import com.dnastack.ddap.common.DdapBy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class WorkflowServersPage extends AnyDdapPage {
+    public WorkflowServersPage(WebDriver driver) {
+        super(driver);
+        waitForInflightRequests();
+        WebElement pageTitle = driver.findElement(DdapBy.se("page-title"));
+        assertThat(pageTitle.getText(), equalTo("Workflows"));
+    }
+
+    public WorkflowListPage clickViewRuns() {
+        waitForInflightRequests();
+        List<WebElement> viewRunsBtns = driver.findElements(DdapBy.se("view-btn"));
+        viewRunsBtns.get(0).click();
+        return new WorkflowListPage(driver, null);
+    }
+
+    public WorkflowManagePage clickManage() {
+        driver.findElement(DdapBy.se("btn-manage"))
+                .click();
+        return new WorkflowManagePage(driver);
+    }
+}
