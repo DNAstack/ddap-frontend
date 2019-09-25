290749524e9cf28a482009f8a6e41a67e7df92ea
Milan Panik <milan.panik1@gmail.com>
2019-09-25 14:07:39 +0200

    DISCO-2437 Removed unused dependencies from pom

--- src/main/java/com/dnastack/ddapfrontend/config/JacksonConfig.java
+++ src/main/java/com/dnastack/ddapfrontend/config/JacksonConfig.java
@@ -2,31 +2,20 @@ package com.dnastack.ddapfrontend.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
-import java.io.IOException;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
-import org.springframework.cloud.openfeign.support.SpringDecoder;
-import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.protobuf.ProtobufEncoder;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import org.springframework.http.converter.protobuf.ProtobufJsonFormatHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -37,21 +26,9 @@ public class JacksonConfig {
         return new ProtobufJsonFormatHttpMessageConverter();
     }
 
-
     @Bean
     public RestTemplate restTemplate(ProtobufHttpMessageConverter hmc) {
         return new RestTemplate(List.of(hmc));
-    }
-
-
-    @Bean
-    public Encoder springEncoder(ObjectFactory<HttpMessageConverters> messageConverters) {
-        return new SpringEncoder(messageConverters);
-    }
-
-    @Bean
-    public Decoder springDecoder(ObjectFactory<HttpMessageConverters> messageConverters) {
-        return new ResponseEntityDecoder(new SpringDecoder(messageConverters));
     }
 
     @Bean
