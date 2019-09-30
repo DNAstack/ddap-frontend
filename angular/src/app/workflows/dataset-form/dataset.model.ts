b7f2984edb5dc7bf0ff4d792436885bf9be22e58
Milan Panik <milan.panik1@gmail.com>
2019-09-30 09:44:10 +0200

    Fix missing fallback 'previous page'

--- angular/src/app/workflows/dataset-form/dataset.model.ts
+++ angular/src/app/workflows/dataset-form/dataset.model.ts
@@ -2,7 +2,8 @@ export interface Dataset {
   schema: {[key: string]: any};
   objects: object[];
   pagination?: {
-    previous_page_url: string;
+    previous_page_url?: string;
+    prev_page_url?: string;
     next_page_url: string;
   };
 }
